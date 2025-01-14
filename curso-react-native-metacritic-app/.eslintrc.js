7a3d177eaf42ad2000c62d7a149c561f51d84e20
Lucas-Noguera <lucas2005n@gmail.com>
2025-01-14 11:20:32 -0300

    Linter Configurado con reglas de React Native

---
+++ curso-react-native-metacritic-app/.eslintrc.js
@@ -0,0 +1,45 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ['expo', 'eslint:recommended', 'plugin:react/recommended', 'plugin:react-native/all'],
+  ignorePatterns: ['/dist/*'],
+  plugins: ['react', 'react-native'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    // Agrega reglas personalizadas si lo deseas
+    'react/react-in-jsx-scope': 'off', // No es necesario importar React en proyectos con React 17+
+    'react/prop-types': 'off', // Si no usas prop-types
+    'react-native/no-unused-styles': 'warn', // Advertencia para estilos no usados
+    'react-native/split-platform-components': 'warn', // Separar componentes de plataforma
+    // Reglas recomendadas de ESLint, TypeScript y React
+    'react-native/no-color-literals': 'off',
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2],
+    'space-in-parens': ['error', 'never'],
+    'react/jsx-uses-react': 'off',
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+    'react/jsx-max-props-per-line': ['error', { maximum: 1, when: 'multiline' }],
+    'react/jsx-closing-bracket-location': ['error', 'tag-aligned'],
+    'react/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }],
+    'react/jsx-curly-spacing': ['error', { when: 'never', children: true }],
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+    'react/self-closing-comp': ['error', { component: true, html: false }],
+  },
+  settings: {
+    react: {
+      version: 'detect', // Detecta automáticamente la versión de React
+    },
+  },
+}
--- curso-react-native-metacritic-app/App.js
+++ curso-react-native-metacritic-app/App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Button, Image, TouchableHighlight } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Image source={{uri: 'https://i.pinimg.com/originals/b5/e9/2a/b5e92a9f9f2e9634b2e816b9594d42f5.jpg'}}
+        style={{width: 215, height: 294}}
+      />
+      <Text style={{fontSize: 20, color: 'white'}}>Metacritic</Text>
+      <Button 
+        title='Pulsa aqui'
+        onPress={() => alert('Hola')}
+      />
+      <TouchableHighlight />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#000',
+    flex: 1,
     justifyContent: 'center',
   },
-});
+})
